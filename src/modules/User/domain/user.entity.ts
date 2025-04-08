feb62eea82fa1a6a363ca258bd8b68b8cf83861e
hadi zare irani <22741265+hadizareirani@users.noreply.github.com>
2025-04-09 00:46:54 +0330

    feat: define User class with properties and password getter

--- src/modules/User/domain/user.entity.ts
+++ src/modules/User/domain/user.entity.ts
@@ -1,3 +1,17 @@
 export class User {
-  constructor() {}
+  constructor(
+    public id: string,
+    public username: string,
+    public email: string,
+    public name: string,
+    private _password: string,
+    public isDeleted: boolean = false,
+    public deletedAt?: Date,
+    public createdAt: Date = new Date(),
+    public updatedAt: Date = new Date(),
+  ) {}
+
+  get password() {
+    return this._password;
+  }
 }
