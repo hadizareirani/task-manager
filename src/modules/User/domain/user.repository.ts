03f96e7d6f0bece68bd191c87535570f0293c739
h.zareirani <h.zareirani@algorock.io>
2025-04-09 08:21:20 +0330

    feat(UserRepository): add CRUD operations for user management
    Add `findByUsername`, `update`, and `delete` methods to the UserRepository interface to support full CRUD operations for user management

--- src/modules/User/domain/user.repository.ts
+++ src/modules/User/domain/user.repository.ts
@@ -1,5 +1,8 @@
 import { User } from './user.entity';
 
 export interface UserRepository {
+  findByUsername(username: string): Promise<User | null>;
   create(user: User): Promise<User>;
+  update(user: User): Promise<User>;
+  delete(id: string): Promise<void>;
 }
