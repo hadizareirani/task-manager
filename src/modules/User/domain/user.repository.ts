f469072235f870a41b020f4faa6f6f3f3f6b0ab6
hadi zare irani <22741265+hadizareirani@users.noreply.github.com>
2025-04-18 01:21:27 +0330

    feat(UserRepository): remove unused update and delete methods from UserRepository interface feat(UserModule): update index to export user repository implementation feat(UserApplication): add index file to export user use cases

--- src/modules/User/domain/user.repository.ts
+++ src/modules/User/domain/user.repository.ts
@@ -5,6 +5,4 @@ export interface UserRepository {
   findById(id: string): Promise<User | null>;
   findFirstUser(username: string, email: string): Promise<User | null>;
   create(user: User): Promise<User>;
-  // update(user: User): Promise<User>;
-  // delete(id: string): Promise<void>;
 }
