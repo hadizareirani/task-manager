591376bb35dec10a3c47c6d604a4c064a072d359
hadi zare irani <22741265+hadizareirani@users.noreply.github.com>
2025-04-18 01:19:28 +0330

    feat(FindUserByIdUseCase): implement use case to find user by ID and handle user not found error

---
+++ src/modules/User/application/find-user-by-id.use-case.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { User, UserRepository } from '../domain';
+import { OperationResponse } from 'src/shared/responses/operation-response';
+import { ErrorListEnum } from 'src/shared/enums/error-list.enum';
+
+@Injectable()
+export class FindUserByIdUseCase {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async execute(id: string) {
+    const user = await this.userRepository.findByUsername(id);
+    if (!user) return OperationResponse.fail(ErrorListEnum.UserNotFound);
+    return OperationResponse.success<User>(user);
+  }
+}
