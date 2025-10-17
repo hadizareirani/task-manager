23e31ab4f1808626669ed18256d3c1fdb5167e65
hadi zare irani <hadizareirani@users.noreply.github.com>
2025-10-17 16:18:08 +0330

    Add ResetPasswordRepository interface for handling reset password operations

---
+++ src/modules/User/domain/repository/reset-password.repository.ts
@@ -0,0 +1,8 @@
+import { Result } from "src/shared/core/result";
+import { ResetPassword } from "../entity";
+import { ErrorListEnum } from "src/shared/enums/error-list.enum";
+
+export interface ResetPasswordRepository {
+    create(resetPassword: ResetPassword): Promise<Result<ResetPassword, ErrorListEnum>>;
+    findByToken(token: string): Promise<Result<ResetPassword, ErrorListEnum>>;
+}
