96050d302b4e8946508c2eb61875b4a8125b9513
Hadi Zare Irani <22741265+hadizareirani@users.noreply.github.com>
2025-11-27 14:57:39 +0330

    refactor: improve code readability in ForgotPasswordUseCase
    - Reformatted import statements for better clarity.
    - Adjusted line breaks for improved readability in the execute method.
    - Enhanced code structure by aligning method calls and conditions.

--- src/modules/Auth/application/forgot-password.use-case.ts
+++ src/modules/Auth/application/forgot-password.use-case.ts
@@ -1,22 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import { CompareUserEmailService, FindUserByUsernameService } from 'src/modules/User';
+import {
+  CompareUserEmailService,
+  FindUserByUsernameService,
+} from 'src/modules/User';
 import { OperationResponse } from 'src/shared/core/operation-response';
-
 
 @Injectable()
 export class ForgotPasswordUseCase {
   constructor(
     private readonly findUserByUsernameService: FindUserByUsernameService,
     private readonly compareUserEmailService: CompareUserEmailService,
-  ) { }
+  ) {}
 
   async execute(username: string, email: string) {
-    const hasUser = await this.findUserByUsernameService.findUserByUsername(username);
+    const hasUser =
+      await this.findUserByUsernameService.findUserByUsername(username);
     if (!hasUser.isSucceeded()) return hasUser.getError();
 
-    const hasValidEmail = this.compareUserEmailService.compare(email, hasUser.getValue().email)
-    if(!hasValidEmail.isSucceeded()) return hasValidEmail.getError();
-    
+    const hasValidEmail = this.compareUserEmailService.compare(
+      email,
+      hasUser.getValue().email,
+    );
+    if (!hasValidEmail.isSucceeded()) return hasValidEmail.getError();
 
     return OperationResponse.success(true);
   }
