8d9dc4a5e85fde3c714d299f015189914c334d54
Hadi Zare Irani <22741265+hadizareirani@users.noreply.github.com>
2025-11-27 15:49:05 +0330

    feat: implement toDomain method in ResetPasswordMapper for mapping raw data to domain entity

--- src/modules/User/infrastructure/mappers/reset-password.mapper.ts
+++ src/modules/User/infrastructure/mappers/reset-password.mapper.ts
@@ -1,13 +1,34 @@
+import { Result } from 'src/shared/core/result';
 import {
   PasswordReset,
   PasswordResetDocument,
 } from '../../schemas/password-reset.schema';
+import { ResetPassword as ResetPasswordEntity } from '../../domain/entity/reset-password.entity';
+import { ErrorListEnum } from 'src/shared/enums/error-list.enum';
+import { Email, Username } from '../../domain';
 
 type ResetPasswordToDomainMapperParams = PasswordReset &
   Pick<PasswordResetDocument, '_id'>;
 
 export class ResetPasswordMapper {
-  static toDomain(raw: ResetPasswordToDomainMapperParams) {
-    return raw as any;
+  static toDomain(
+    raw: ResetPasswordToDomainMapperParams,
+  ): Result<ResetPasswordEntity, ErrorListEnum> {
+    const username = Username.fromPersistence(raw.username);
+    const email = Email.fromPersistence(raw.email);
+
+    return Result.ok(
+      ResetPasswordEntity.create({
+        id: raw._id.toString(),
+        userId: raw.userId.toString(),
+        resetToken: raw.resetToken,
+        username,
+        email,
+        expiresAt: raw.expiresAt,
+        isUsed: raw.isUsed,
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      }),
+    );
   }
 }
