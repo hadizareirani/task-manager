910a8acc09c2649f43a61636f7857a887ebacb0f
Hadi Zare Irani <22741265+hadizareirani@users.noreply.github.com>
2025-12-05 11:34:21 +0330

    feat: refactor ForgotPasswordValidationUseCase and implement token validation logic in ResetPasswordValidationService

--- src/modules/Auth/application/use-cases/forgot-password-validation.use-case.ts
+++ src/modules/Auth/application/use-cases/forgot-password-validation.use-case.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import {
-  CompareUserEmailService,
-  CreateResetPasswordService,
-  FindUserByUsernameService,
-} from 'src/modules/User';
+import { ResetPasswordValidationService } from 'src/modules/User';
 import { OperationResponse } from 'src/shared/core/operation-response';
 
 @Injectable()
 export class ForgotPasswordValidationUseCase {
   constructor(
-    private readonly findUserByUsernameService: FindUserByUsernameService,
-    private readonly compareUserEmailService: CompareUserEmailService,
-    private readonly createResetPasswordService: CreateResetPasswordService,
+    private readonly resetPasswordValidationService: ResetPasswordValidationService,
   ) {}
 
-  execute(token: string) {
+  async execute(token: string) {
+    // Todo
+    const isValidToken =
+      await this.resetPasswordValidationService.ResetPasswordValidation(token);
+    if (!isValidToken.isSucceeded())
+      return OperationResponse.fail(isValidToken.getError());
+
     return OperationResponse.success(true);
   }
 }
--- src/modules/User/application/services/reset-password/reset-password-validation.service.ts
+++ src/modules/User/application/services/reset-password/reset-password-validation.service.ts
@@ -6,9 +6,10 @@ import {
   ResetPasswordRepository,
   // Username,
 } from '../../../domain';
-// import { OperationResponse } from 'src/shared/core/operation-response';
+import { OperationResponse } from 'src/shared/core/operation-response';
 // import { ErrorListEnum } from 'src/shared/enums/error-list.enum';
 import { RESET_PASSWORD_REPOSITORY } from '../../../constants';
+import { ErrorListEnum } from 'src/shared/enums/error-list.enum';
 // import { ResetPassword } from '../../domain/entity/reset-password.entity';
 
 @Injectable()
@@ -17,5 +18,19 @@ export class ResetPasswordValidationService {
     @Inject(RESET_PASSWORD_REPOSITORY)
     private readonly resetPasswordRepository: ResetPasswordRepository,
   ) {}
-  ResetPasswordValidation(token: string) {}
+
+  async ResetPasswordValidation(token: string) {
+    const resetPasswordOrError =
+      await this.resetPasswordRepository.findByToken(token);
+
+    if (resetPasswordOrError.isFailure)
+      return OperationResponse.fail(resetPasswordOrError.getError());
+
+    const resetPassword = resetPasswordOrError.getValue();
+
+    if (resetPassword.expiresAt < new Date() || resetPassword.isUsed)
+      return OperationResponse.fail(ErrorListEnum.TokenIsInvalid);
+    // Todo
+    return OperationResponse.success(true);
+  }
 }
